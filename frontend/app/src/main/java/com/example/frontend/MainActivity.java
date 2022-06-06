54414a6e38a25840bcd4d6feef68cae807d60f8f
Richard Binder <binder.richard@outlook.de>
2022-06-06 05:14:45 +0200

    #29 Add remember user after login

--- frontend/app/src/main/java/com/example/frontend/MainActivity.java
+++ frontend/app/src/main/java/com/example/frontend/MainActivity.java
@@ -6,7 +6,14 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.accounts.AbstractAccountAuthenticator;
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.AccountManager;
+import android.accounts.NetworkErrorException;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,9 +33,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        UserSchema rememberedUser = getRememberedUser();
+
         MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
         EditText username = (EditText)findViewById(R.id.username);
         EditText password = (EditText)findViewById(R.id.password);
+
+        if(rememberedUser != null) {
+            username.setText(rememberedUser.getUsername());
+            password.setText(rememberedUser.getPassword());
+        }
+
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,10 +57,34 @@ public class MainActivity extends AppCompatActivity {
         if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
             Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show();
             Log.i(this.getClass().getName(), "Login successful");
+            rememberUser(username.getText().toString(), password.getText().toString());
             enableCamera();
         } else {
             findViewById(R.id.notification).setVisibility(View.VISIBLE);
             Log.i(this.getClass().getName(), "Login failed");
+        }
+    }
+
+    private void rememberUser(String username, String password) {
+        SharedPreferences sp=getSharedPreferences("Login", MODE_PRIVATE);
+        SharedPreferences.Editor Ed=sp.edit();
+        Ed.putString("username",username );
+        Ed.putString("password",password);
+        Ed.commit();
+    }
+
+    private UserSchema getRememberedUser() {
+        SharedPreferences sp1=this.getSharedPreferences("Login", MODE_PRIVATE);
+
+        if (sp1.contains("username") && sp1.contains("password")) {
+            String username=sp1.getString("username", null);
+            String password = sp1.getString("password", null);
+
+            Log.i(this.getClass().getName(), "Retrieved remembered user: "  + username );
+            return new UserSchema(username, password);
+        } else {
+            Log.i(this.getClass().getName(), "No remembered user");
+            return null;
         }
     }
 
