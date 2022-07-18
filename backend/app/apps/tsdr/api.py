2f50fad1ab3ba9d26c2bf8a2af6dbfb6def6b076
Richard Binder <binder.richard@outlook.de>
2022-07-18 19:01:55 +0200

    #40 Remove test api method, not needed anymore

--- backend/app/apps/tsdr/api.py
+++ backend/app/apps/tsdr/api.py
@@ -14,10 +14,3 @@ def traffic_sign_detection(request, id: int, image_file: UploadedFile = File(...
     requestLogger.info('tsdr traffic image: ' + str(id) + ", name: " + image_file.name + ', bytesize: ' + str(image_file.size), extra={'request': request})
     services.tsd_file(image_file)
     return id
-
-
-@router.get('/test', auth=None)
-def test(request):
-    requestLogger.info('test', extra={'request': request})
-
-    return 0
