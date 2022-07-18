ba2155a979b15b662b6f1309a43958ba259566be
Richard Binder <binder.richard@outlook.de>
2022-07-18 16:36:24 +0200

    #35 Generalize getResponseTime method

--- frontend/app/src/main/java/com/example/frontend/util/NetworkMeasures.java
+++ frontend/app/src/main/java/com/example/frontend/util/NetworkMeasures.java
@@ -4,7 +4,7 @@ import okhttp3.ResponseBody;
 import retrofit2.Response;
 
 public class NetworkMeasures {
-    public static long getResponseTime(Response<ResponseBody> response) {
+    public static long getResponseTime(Response<?> response) {
         long requestTime = response.raw().sentRequestAtMillis();
         long responseTime = response.raw().receivedResponseAtMillis();
         //time taken to receive the response after the request was sent
