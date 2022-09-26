21eaf8d54d7eb6de0bbbe7651937bf28e29b2907
Richard Binder <binder.richard@outlook.de>
2022-09-26 22:34:31 +0200

    #13 Added a project to train and use a TSR model

---
+++ tsrresnet/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+def get_install_requirements():
+    with open("requirements.txt", "r", encoding="utf-8") as f:
+        reqs = [x.strip() for x in f.read().splitlines()]
+    reqs = [x for x in reqs if not x.startswith("#")]
+    return reqs
+
+
+setuptools.setup(
+    name="tsrresnet",
+    version="1.0.0",
+    author="Richard Binder",
+    packages=["tsrresnet.tools"],
+    package_dir={"tsrresnet.tools": "tsrresnet/tools"},
+    python_requires=">=3.8",
+    install_requires=get_install_requirements(),
+    setup_requires=["wheel"],  # avoid building error when pip is not updated
+    include_package_data=True,  # include files in MANIFEST.in
+)
