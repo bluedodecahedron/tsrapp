917c73026006c612f0b430e0fc445a1b8ae82ec7
Richard Binder <binder.richard@outlook.de>
2022-09-28 00:49:30 +0200

    #14 Fix missing output variable in tests

--- backend/app/apps/tsdr/tests.py
+++ backend/app/apps/tsdr/tests.py
@@ -6,10 +6,10 @@ import cv2
 class TsdrTestCase(TestCase):
     def test_tsdr_detects_many_classes(self):
         image = cv2.imread("resources/manytrafficsigns.jpg")
-        class_ids = services.tsdr(image)
+        class_ids, img = services.tsdr(image)
         assert len(class_ids) == 9
 
     def test_tsdr_detects_no_classes(self):
         image = cv2.imread("resources/notrafficsigns.jpg")
-        class_ids = services.tsdr(image)
+        class_ids, img = services.tsdr(image)
         assert len(class_ids) == 0
